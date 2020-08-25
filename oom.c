595991257306ff86a3516fb13d7424df287738a5
Kevin Orr <kevinorr54@gmail.com>
2020-08-25 02:15:58 -0400

    Better output

--- oom.c
+++ oom.c
@@ -5,6 +5,8 @@ int main() {
     char *p;
     size_t amount = 1;
 
+    setvbuf(stdout, NULL, _IONBF, 0);
+
     while(1) {
         printf("Allocating: %zu bytes\n", amount);
         p = (char*) calloc(1, amount);
